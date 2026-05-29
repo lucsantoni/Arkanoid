8fe771fc110080274641d860f4a1c5757b51954d
Lucas Antoni <lucas.antoni@ufrgs.br>
2026-05-29 11:30:05 -0300

    att aula 29/05

--- main.c
+++ main.c
@@ -50,12 +50,13 @@ void DesenhaMenu(int selecionada) {
     };
     int espacamento = 75;
     int yinicial = 220;
+    
     for (int i = 0; i < OPCOES; i++) {
         char texto[30];
         if (i == selecionada) {
-            sprintf(texto, "[ %s ]", opcoes[i]);
+            sprintf(texto, "[ %s ]", opcoes[i]); //coloca [] pra destacar a op selecionada
         } else {
-            sprintf(texto, "%s", opcoes[i]);
+            sprintf(texto, "%s", opcoes[i]); //deixa as outras sem destaque
         }
         int larguratexto = MeasureText(texto, TAMANHO_FONTE);
         int x = (LARGURA - larguratexto) / 2;
@@ -73,28 +74,28 @@ void DesenhaBola(Bola bola) {
 }
 
 void DesenhaJogador(Jogador jogador){
-DrawText(TextFormat("Vidas: %d Pontos: %d", jogador.vidas, jogador.pontos), 20, 10, 20, WHITE);
+    DrawText(TextFormat("Vidas: %d Pontos: %d", jogador.vidas, jogador.pontos), 20, 10, 20, WHITE);
 }
 
 void MovePlataforma(Plataforma *plat) {
-    if (IsKeyDown(KEY_LEFT)) plat->x -= VELPLATAFORMA;
-    if (IsKeyDown(KEY_RIGHT)) plat->x += VELPLATAFORMA;
+    if (IsKeyDown(KEY_LEFT)) plat->x -= VELPLATAFORMA; // move para a esquerda
+    if (IsKeyDown(KEY_RIGHT)) plat->x += VELPLATAFORMA; // move para a direita
 
-    if (plat->x < 0) plat->x = 0;
-    if (plat->x > (LARGURA - plat->larg)) plat->x = LARGURA - plat->larg;
+    if (plat->x < 0) plat->x = 0; // impede que a plataforma ultrapasse a borda esquerda
+    if (plat->x > (LARGURA - plat->larg)) plat->x = LARGURA - plat->larg; // impede que a plataforma ultrapasse a borda direita
 }
 
 void MoveBola(Bola *bola, Plataforma *plat) {
-    if (bola->ativa == 1) {
-        bola->x += bola->dx;
-        bola->y += bola->dy;
+    if (bola->ativa == 1) { // só move a bola se ela estiver ativa
+        bola->x += bola->dx; // move a bola na direção horizontal, add velocidade horizontal
+        bola->y += bola->dy; // move a bola na direção vertical, add velocidade vertical
 
-        if (bola->x - RAIOBOLA <= 0 || bola->x + RAIOBOLA >= LARGURA) bola->dx = -bola->dx;
-        if (bola->y - RAIOBOLA <= 0) bola->dy = -bola->dy;
+        if (bola->x - RAIOBOLA <= 0 || bola->x + RAIOBOLA >= LARGURA) bola->dx = -bola->dx; // inverte a direção horizontal se bater nas paredes laterais
+        if (bola->y - RAIOBOLA <= 0) bola->dy = -bola->dy; // inverte a direção vertical se bater no teto
 
 
-        if (bola->y + RAIOBOLA >= plat->y && bola->y + RAIOBOLA <= plat->y + ALTPLATAFORMA &&
-            bola->x >= plat->x && bola->x <= plat->x + plat->larg && bola->dy > 0) {
+        if (bola->y + RAIOBOLA >= plat->y && bola->y + RAIOBOLA <= plat->y + ALTPLATAFORMA && // verifica se a bola está na altura da plataforma
+            bola->x >= plat->x && bola->x <= plat->x + plat->larg && bola->dy > 0) { // 
 
             float ponto_colisao = (bola->x - plat->x) / plat->larg;
             float velocidade = sqrt((bola->dx) * (bola->dx) + (bola->dy) * (bola->dy));
@@ -158,14 +159,84 @@ void DesenhaTijolos(Tijolo tijolos[], int quantidade) {
     }
 }
 
+void ColisaoBolaTijolo(Bola *bola, Tijolo tijolos[], int quantidade, Jogador *jogador) {
+
+    for (int i = 0; i < quantidade; i++) {
+
+        if (tijolos[i].ativo) {
+
+            if (bola->x + RAIOBOLA >= tijolos[i].x &&
+                bola->x - RAIOBOLA <= tijolos[i].x + tijolos[i].larg &&
+                bola->y + RAIOBOLA >= tijolos[i].y &&
+                bola->y - RAIOBOLA <= tijolos[i].y + tijolos[i].alt) {
+
+                tijolos[i].ativo = 0;
+
+                bola->dy = -bola->dy;
+
+                jogador->pontos += 100;
+
+                break;
+            }
+        }
+    }
+}
+
+void SalvaJogo(Jogador jogador, Tijolo tijolos[], int quantidade) {
+    FILE *arq = fopen("save.dat", "wb");
+
+    if (arq == NULL)
+    {
+        printf("Erro ao criar save!\n");
+        return;
+    }
+
+    fwrite(&jogador, sizeof(Jogador), 1, arq);
+    fwrite(tijolos, sizeof(Tijolo), quantidade, arq);
+
+    fclose(arq);
+}
+
+void CarregaJogo(Jogador *jogador, Tijolo tijolos[], int quantidade){
+    FILE *arq = fopen("save.dat", "rb");
+
+    if (arq == NULL)
+    {
+        printf("Nenhum save encontrado!\n");
+        return;
+    }
+
+    fread(jogador, sizeof(Jogador), 1, arq);
+    fread(tijolos, sizeof(Tijolo), quantidade, arq);
+
+    fclose(arq);
+}
+
+void ReinicializaJogo(Jogador *jogador, Tijolo tijolos[], Plataforma *plat, Bola *bola, int qtdTijolos) {
+    jogador->vidas = 3;
+    jogador->pontos = 0;
+
+    plat->x = 270.0f;
+    plat->y = 560.0f;
+    plat->larg = 100.0f;
+
+    bola->x = plat->x + plat->larg / 2.0f;
+    bola->y = plat->y - RAIOBOLA;
+    bola->dx = 3.0f;
+    bola->dy = -3.0f;
+    bola->ativa = 0;
+
+    InicializaTijolos(tijolos, qtdTijolos);
+}
+
 int main(void) {
     InitWindow(LARGURA, ALTURA, "ARKANOID");
 
     InitAudioDevice();
 
-    Music musica = LoadMusicStream("musica_maneira.mp3");
-    Texture2D imgInicio = LoadTexture("fundoinicio.png");
-    Texture2D imgJogo = LoadTexture("fundojogo.png");
+    Music musica = LoadMusicStream("musica_maneira.mp3"); //
+    Texture2D imgInicio = LoadTexture("fundoinicio.png"); // 
+    Texture2D imgJogo = LoadTexture("fundojogo.png"); 
 
     PlayMusicStream(musica);
 
@@ -173,18 +244,26 @@ int main(void) {
 
     int selecionada = 0;
     int tela = 0;
+    int tempoMensagemSave = 0;
 
     Plataforma plataforma = { 270.0f, 560.0f, 100.0f };
     Bola bola = { 320.0f, 550.0f, 3.0f, -3.0f, 0 };
     Jogador jogador = { 3, 0 };
 
-    Tijolo tijolos[QTDTIJOLOS];
-    InicializaTijolos(tijolos, QTDTIJOLOS);
+    Tijolo tijolos[QTDTIJOLOS]; // array de tijolos
+    InicializaTijolos(tijolos, QTDTIJOLOS); // inicializa os tijolos
 
-    while (!WindowShouldClose()) {
+while (!WindowShouldClose()) {
+
+    UpdateMusicStream(musica);
+
+    if (tempoMensagemSave > 0){
+        tempoMensagemSave--;
+    }
+
         if (tela == 0) {
             if (IsKeyPressed(KEY_UP)) {
-                pointer: selecionada--;
+                selecionada--;
                 if (selecionada < 0) selecionada = OPCOES - 1;
             }
             if (IsKeyPressed(KEY_DOWN)) {
@@ -194,10 +273,13 @@ int main(void) {
             if (IsKeyPressed(KEY_ENTER)) {
                 switch (selecionada) {
                     case 0:
+                        SalvaJogo(jogador, tijolos, QTDTIJOLOS);
+                        ReinicializaJogo(&jogador, tijolos, &plataforma, &bola, QTDTIJOLOS);
                         tela = 1;
                         break;
                     case 1:
-                        printf("Carregar Jogo selecionado\n");
+                        CarregaJogo(&jogador, tijolos, QTDTIJOLOS);
+                        tela = 1;
                         break;
                     case 2:
                         printf("Opcoes selecionadas\n");
@@ -210,46 +292,57 @@ int main(void) {
             }
         }
 
-        if (tela == 1) {
-            MovePlataforma(&plataforma);
-
-            if (bola.ativa == 0) {
-                bola.x = plataforma.x + plataforma.larg / 2.0f;
-                bola.y = plataforma.y - RAIOBOLA;
-                if (IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT)) {
-                    bola.ativa = 1;
-                }
-            }
-
-            MoveBola(&bola, &plataforma);
-        }
-
-        BeginDrawing();
-            ClearBackground(BLACK);
-
-            if (tela == 0) {
-                DrawTexture(imgInicio, 0, 0, WHITE);
-                DesenhaMenu(selecionada);
-                UpdateMusicStream(musica);
-            }
-
-            if (tela == 1) {
-                ClearBackground(BLACK);
-                DrawTexture(imgJogo, 0, 0, WHITE);
-
-                DesenhaPlataforma(plataforma);
-                DesenhaBola(bola);
-                DesenhaJogador(jogador);
-                DesenhaTijolos(tijolos, QTDTIJOLOS);
-
-            if (IsKeyPressed(KEY_P)) {
-                tela = 0;
-            }
-
-            }
-        EndDrawing();
+        if (tela == 1)
+{
+    if (IsKeyPressed(KEY_S)){
+        SalvaJogo(jogador, tijolos, QTDTIJOLOS);
+        tempoMensagemSave = 180;
     }
 
+    MovePlataforma(&plataforma);
+
+    if (bola.ativa == 0){
+        bola.x = plataforma.x + plataforma.larg / 2.0f;
+        bola.y = plataforma.y - RAIOBOLA;
+
+        if (IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT)){
+            bola.ativa = 1;
+        }
+    }
+
+    MoveBola(&bola, &plataforma);
+    ColisaoBolaTijolo(&bola, tijolos, QTDTIJOLOS, &jogador);
+}
+
+BeginDrawing();
+ClearBackground(BLACK);
+
+if (tela == 0){
+    DrawTexture(imgInicio, 0, 0, WHITE);
+    DesenhaMenu(selecionada);
+}
+
+if (tela == 1){
+    DrawTexture(imgJogo, 0, 0, WHITE);
+
+    DesenhaPlataforma(plataforma);
+    DesenhaBola(bola);
+    DesenhaJogador(jogador);
+    DesenhaTijolos(tijolos, QTDTIJOLOS);
+
+    if (tempoMensagemSave > 0){
+        DrawText("JOGO SALVO", 450, 20, 20, WHITE);
+    }
+
+    if (IsKeyPressed(KEY_P)){
+        tela = 0;
+    }
+}
+
+EndDrawing();
+
+}
+    
     UnloadTexture(imgInicio);
     UnloadTexture(imgJogo);
     UnloadMusicStream(musica);
